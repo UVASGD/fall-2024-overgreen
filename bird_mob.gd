7d9f402e3c78f01f280c8b29b0fbb3c3ee341eca
csj606 <139289685+csj606@users.noreply.github.com>
2024-10-03 20:03:42 -0400

    Added Movement
    Added vector movement to bird mob

--- bird_mob.gd
+++ bird_mob.gd
@@ -1,4 +1,11 @@
 extends CharacterBody2D
 
 const speed = 30;
-var dir: Vector2
+var dir: Vector2;
+
+func _on_timer_timeout():
+	$Timer.wait_time = 3;
+	if Vector2.UP:
+		Vector2.DOWN;
+	else:
+		Vector2.UP;
