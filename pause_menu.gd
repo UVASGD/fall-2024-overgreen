424bfbb1fb7dffbf18b0cab9fe3d2e7df96371c6
Brant Willis <bwillie215@gmail.com>
2024-10-06 18:55:35 -0400

    starting to add pause menu:

---
+++ pause_menu.gd
@@ -0,0 +1,23 @@
+extends Control
+
+func resume():
+	get_tree().paused = false
+	print("resumed")
+	$AnimationPlayer.play_backwards("blur")
+	
+func pause():
+	get_tree().paused = true;
+	print("paused")
+	$AnimationPlayer.play("blur")
+	
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("esc") and !get_tree().paused:
+		pause()
+	elif Input.is_action_just_pressed("esc") and get_tree().paused:
+		resume()
+
+func _on_resume_pressed() -> void:
+	resume()
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
