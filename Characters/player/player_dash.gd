11caa9ef2768ddcb0a571d26b172ce2a6e925c27
Max Calcoen <maxcalcoen@gmail.com>
2024-10-07 14:01:53 -0400

    added velocity curve to dash and added ability to cancel out of dash last 30ms

--- Characters/player/player_dash.gd
+++ Characters/player/player_dash.gd
@@ -15,21 +15,21 @@ var dashing = false
 var dash_speed: float
 var dash_time: float
 
-func _ready():
+func _ready() -> void:
 	player = get_parent()
 
-func set_constants(dash_speed_in, dash_time_in, dashes_in):
+func set_constants(dash_speed_in: float, dash_time_in: float, dashes_in: float) -> void:
 	dash_speed = dash_speed_in
 	dash_time = dash_time_in
 	dash_default_num_charges = dashes_in
 	dash_charges = dashes_in
 
 # returns if player can dash
-func _get_dash():
-	return dash_charges >= 1.0
+func _get_dash() -> bool:
+	return dash_charges >= 1.0 && player.direction != Vector2.ZERO
 
 # dash tick
-func tick():
+func tick() -> void:
 	if Input.is_action_just_pressed("dash"):
 		# currently not implemented: dash hold for long dash
 		if _get_dash():
@@ -40,7 +40,7 @@ func tick():
 
 
 # dash logic
-func dash():
+func dash() -> void:
 	dash_charges -= 1
 	# lock movement
 	emit_signal("lock_movement", true)
@@ -51,13 +51,24 @@ func dash():
 	player.velocity = direction * dash_speed
 	dashing = true
 	var elapsed_time = 0.0
-	while elapsed_time < dash_time and not player.is_on_floor():
+	while elapsed_time < dash_time:
 		await get_tree().process_frame
 		elapsed_time += get_process_delta_time()
+		player.velocity = speed_at_time(elapsed_time * 1000.0, dash_speed, player.speed, dash_time) * direction
+		# regain control 30 ms before dash ends or if dashes into floor
+		if dash_time - elapsed_time < 0.03 || player.is_on_floor() && elapsed_time > 0.1:
+			emit_signal("lock_movement", false)
 	dashing = false
-	emit_signal("lock_movement", false)
 
+func speed_at_time(t: float, v0: float, vf: float, T: float) -> float:
+	# calculate constants A and C based on v0 and vf
+	var A = (v0 - vf) / 2.0
+	var C = (v0 + vf) / 2.0
 
-# returns if character is dashing
-func is_dashing():
-	return dashing
+	# angular frequency ω (half cosine wave)
+	var omega = PI / T
+
+	# compute velocity using the cosine equation
+	# https://www.desmos.com/calculator/vin4sytkdf
+	var v_t = A * cos(omega * t) + C
+	return v_t
