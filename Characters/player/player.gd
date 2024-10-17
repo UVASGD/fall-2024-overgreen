48ddc772bf0db5e302d9b633288b980686e0c9b7
abelsaberhagen <abelsaberhagen@gmail.com>
2024-10-17 19:08:57 -0400

    grapple v1

--- Characters/player/player.gd
+++ Characters/player/player.gd
@@ -11,11 +11,39 @@ extends CharacterBody2D
 @onready var animation_handler: SpriteAnimation = SpriteAnimation.new(animated_sprite)
 
 var direction: Vector2 = Vector2.ZERO
+var theta
+var isGrappling
+var radius = 10.0
+
+func _ready():
+	theta = -3
+	isGrappling = false
 
 func _physics_process(delta):
-	basic_movement_handler.tick(delta)
+	var t = 0
+	if isGrappling == false:
+		basic_movement_handler.tick(delta)
+	else:
+		position.x = radius * cos(theta)
+		position.y = radius * sin(theta)
+		
+		
+		if Input.is_action_pressed("left"):
+			theta = 0
+		if Input.is_action_pressed("right"):
+			theta = 180
+		if Input.is_action_pressed("up"):
+			theta = 90
+		if Input.is_action_pressed("down"):
+			theta = 270
+		
+	
+	if Input.is_action_just_pressed("grappling"):
+		isGrappling = !isGrappling
+		
 	animation_handler.tick(direction)
-
 
 func land():
 	animation_handler.set_state("jump_end")
+
+ 
