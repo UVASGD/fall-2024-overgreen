e9a9d54b8bee1c75ea7b1f1f4fb1aca7386f3e30
dsizzle67 <ahsokabooks@outlook.com>
2024-10-24 12:04:30 -0400

    double jump fixed

--- Characters/player/player_basic_movement.gd
+++ Characters/player/player_basic_movement.gd
@@ -29,6 +29,7 @@ func tick(delta):
 		player.velocity.y += gravity * delta
 		was_in_air = true
 	elif was_in_air:
+		has_double_jumped = false
 		# todo: change to signal method
 		player.land()
 		was_in_air = false
