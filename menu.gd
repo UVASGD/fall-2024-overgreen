29b1ab5f1cdc6f250583fca1471288371a197e0f
Brant Willis <bwillie215@gmail.com>
2024-11-12 10:06:14 -0500

    start menu animation

--- menu.gd
+++ menu.gd
@@ -1,6 +1,11 @@
 extends Control
+@export var pos = Vector2(357, 75.18)
 
-
+func _process(delta: float) -> void:
+	get_node("PanelContainer/Sprite2D").position -= Vector2(.25,0)
+	pos = get_node("PanelContainer/Sprite2D").position
+	if get_node("PanelContainer/Sprite2D").position == Vector2(26, 75.18):
+		get_node("PanelContainer/Sprite2D").position = Vector2(357, 75.18)
 
 func _on_start_new_pressed() -> void:
 	get_tree().change_scene_to_file("res://Levels/test_level.tscn")
--- options_menu.gd
+++ options_menu.gd
@@ -1,4 +1,10 @@
 extends Control
 
+
+func _process(delta: float) -> void:
+	get_node("PanelContainer/Sprite2D").position += Vector2(.25,0)
+	if get_node("PanelContainer/Sprite2D").position == Vector2(357, 75.18):
+		get_node("PanelContainer/Sprite2D").position = Vector2(26, 75.18)
+
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://start_menu.tscn")
